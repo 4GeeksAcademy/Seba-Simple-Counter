2fa159209ff75b87b2e8e457ccf36acc571eb0da
Sebastian Valdes <seba.valdes.es@gmail.com>
2023-12-13 23:59:24 -0300

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import './index.css'; // Importa el archivo CSS
 
-function App() {
+const Contador = () => {
+  const [segundos, setSegundos] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setSegundos(segundos => segundos + 1);
+    }, 1000);
+
+
+    return () => clearInterval(intervalo);
+  }, []);
+
+
+  const formatoSegundos = String(segundos).padStart(6, '0');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="relojTitulo">
+        Contador de segundos:
+      </h1>
+      <div className="contadorNumeroContainer">
+        <FontAwesomeIcon icon={faClock} className="relojIcono" />
+        {formatoSegundos.split('').map((digito, index) => (
+          <div key={index} className="contadorNumero">
+            {digito}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Contador;
